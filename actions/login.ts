b635334db973df856dc00b04a9bdf5503ebe4356
Hrithik Agarwal <hrithikinfinite@gmail.com>
2024-09-08 22:24:59 +0530

    added signin logic

--- actions/login.ts
+++ actions/login.ts
@@ -1,5 +1,8 @@
 "use server";
+import { signIn } from "@/auth";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 import { LoginSchema } from "@/schemas";
+import { AuthError } from "next-auth";
 import * as z from "zod";
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
@@ -7,5 +10,24 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
   if (!validateFields.success) {
     return { error: "Invalid Fields!" };
   }
-  return { success: "Email Sent!" };
+  const { email, password } = validateFields.data;
+  try {
+    await signIn("credentials", {
+      email,
+      password,
+      redirectTo: DEFAULT_LOGIN_REDIRECT,
+    });
+    console.log(`Login Success for ${email}`);
+  } catch (error) {
+    console.log(`Error while signing in :${JSON.stringify(error)}`);
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return { error: "Invalid credentials" };
+        default:
+          return { error: "Something went wrong!" };
+      }
+    }
+    throw error;
+  }
 };
--- components/auth/register-form.tsx
+++ components/auth/register-form.tsx
@@ -29,8 +29,8 @@ export const RegisterForm = () => {
     setSuccess("");
     startTransition(() => {
       register(values).then((data) => {
-        setError(data.error);
-        setSuccess(data.success);
+        setError(data?.error);
+        setSuccess(data?.success);
       });
     });
   };
