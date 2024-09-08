48183cccb97853e35aa213c7a3ca89ca6f3c29cb
Hrithik Agarwal <hrithikinfinite@gmail.com>
2024-09-08 20:59:31 +0530

    added registration to db

--- actions/register.ts
+++ actions/register.ts
@@ -1,11 +1,29 @@
 "use server";
 import { RegisterSchema } from "@/schemas";
 import * as z from "zod";
-
+import bcrypt from "bcrypt";
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validateFields = RegisterSchema.safeParse(values);
   if (!validateFields.success) {
     return { error: "Invalid Fields!" };
   }
-  return { success: "Email Sent!" };
+  const { email, password, name } = validateFields.data;
+  const hashedPswd = await bcrypt.hash(password, 10);
+  const isExisiting = await getUserByEmail(email);
+  if (isExisiting) {
+    return { error: "Email already taken!" };
+  }
+  console.log({ name, email, password, hashedPswd });
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPswd,
+    },
+  });
+
+  //TODO : Send Verification toekn Email
+  return { success: "User Created!" };
 };
