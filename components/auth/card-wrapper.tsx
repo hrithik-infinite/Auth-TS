3eb6a115284b0bde47f76f1ac864f29dcf16960c
Hrithik Agarwal <hrithikinfinite@gmail.com>
2024-09-08 13:23:40 +0530

    added basic routing and layout

--- components/auth/card-wrapper.tsx
+++ components/auth/card-wrapper.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import { Card, CardContent, CardFooter, CardHeader } from "../ui/card";
 import { Header } from "./header";
 import { Social } from "./social";
+import BackButton from "./back-button";
 
 interface CardWrapperProps {
   children: React.ReactNode;
@@ -24,6 +25,9 @@ export const CardWrapper = ({ children, headerLabel, backButtonLabel, backButton
           <Social />
         </CardFooter>
       )}
+      <CardFooter>
+        <BackButton href={backButtonHref} label={backButtonLabel} />
+      </CardFooter>
     </Card>
   );
 };
