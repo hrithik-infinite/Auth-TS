6353abf5bb896df360967ca6612056c4bdeac1e7
Hrithik Agarwal <hrithikinfinite@gmail.com>
2024-09-11 20:48:44 +0530

    addded reset pswd route and basic layout

--- routes.ts
+++ routes.ts
@@ -1,4 +1,4 @@
 export const publicRoutes = ["/", "/auth/new-verification"];
-export const authRoutes = ["/auth/login", "/auth/register", "/auth/error"];
+export const authRoutes = ["/auth/login", "/auth/register", "/auth/error", "/auth/reset"];
 export const apiAuthPrefix = "/api/auth";
 export const DEFAULT_LOGIN_REDIRECT = "/settings";
