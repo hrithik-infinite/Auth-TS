9a1ec4368a910aefd19485022d0f6630a1d09129
Hrithik Agarwal <hrithikinfinite@gmail.com>
2024-09-17 07:31:23 +0530

    build failure fixes

--- actions/settings.ts
+++ actions/settings.ts
@@ -3,19 +3,24 @@ import { auth } from "@/auth";
 import { getUserById } from "@/data/user";
 import { db } from "@/lib/db";
 import { SettingsSchema } from "@/schemas";
-import { User } from "@prisma/client";
 import * as z from "zod";
 
 export const settings = async (values: z.infer<typeof SettingsSchema>) => {
   const session = await auth();
 
-  const user = session?.user as User;
+  const user = session?.user;
   if (!user) {
     return { error: "Unauthorized" };
   }
-  const dbUser = await getUserById(user.id);
+  const dbUser = await getUserById(user?.id ? user.id : "");
   if (!dbUser) {
     return { error: "Unauthorized" };
+  }
+  if (user.isOAuth) {
+    values.email = undefined;
+    values.password = undefined;
+    values.newPassword = undefined;
+    values.is2faEnabled = undefined;
   }
   await db.user.update({
     where: { id: dbUser.id },
